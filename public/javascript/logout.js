cc6cfadc225dcb19b4349d63b6bbba0be5cbbf3e
rakky21 <rakky26@gmail.com>
2021-12-05 13:07:43 -0500

    added the style on the main page and added the session timers to log you out if you are idle for too long

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -3,12 +3,17 @@ async function logout() {
     method: 'post',
     headers: { 'Content-Type': 'application/json' }
   });
-// if iddle for too long. log out.
+// if iddle for too long. log out or if closing app
   if (response.ok) {
     document.location.replace('/');
   } else {
     alert(response.statusText);
   }
 }
+
+// logout timer
+setInterval(function() {
+logout()
+}, 30000)
 
 document.querySelector('#logout').addEventListener('click', logout);
